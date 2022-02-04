a292d0c85344a4aa2b5a686bb5e4982aa884aed9
JoshGenereux <josh.genereux13@gmail.com>
2022-02-04 12:01:42 -0700

    got maps working, api working

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -7,11 +7,10 @@ import Rating from '@material-ui/lab';
 import useStyles from './styles';
 import {LocationOnOutlined} from "@material-ui/icons";
 
-const Map = ()=>{
+const Map = ({coordinates, setCoordinates, setBounds})=>{
   const classes = useStyles();
   const isMobile = useMediaQuery('(min-width:600px')
 
-  const coordinates = { lat: 0, lng: 0};
 
   return (
     <div className={classes.mapContainer}>
@@ -22,8 +21,11 @@ const Map = ()=>{
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
         options={''}
-        onChange={''}
-        onChildClick={''}
+        onChange={(e)=>{
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng})
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw})
+        }}
+        // onChildClick={''}
       />
     </div>
   )
